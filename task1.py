91636194f554036e0e685c726a977e0b66299ead
Mayank685 <84919783+Mayank685@users.noreply.github.com>
2021-05-27 21:21:16 +0530

    Task1 My caption

---
+++ task1.py
@@ -0,0 +1,11 @@
+# import math module
+from math import pi
+
+# take input from user
+r = float(input ("Input the radius of the circle :1.1 "))
+
+# compute the area from radius of a circle given by user
+calculateArea = str(pi * r**2);
+
+#print result
+print ("The area of the circle with radius: 1.1 " + str(r) + " is: " + calculateArea)
